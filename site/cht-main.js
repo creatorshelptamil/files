3f067d520edd78b2d5693f931c3f9590453c3442
Creators Help Tamil <74766168+creatorshelptamil@users.noreply.github.com>
2021-07-18 15:05:59 +0530

    Create cht-main.js

---
+++ site/cht-main.js
@@ -0,0 +1,16 @@
+(function(){
+//empty var
+var test;
+
+var arr = [];
+
+arr.push(1);
+arr.push(2);
+
+var str = "Hello";
+
+var el = document.querySelector(".el");
+
+el.innerHTML = "" + str.substring(0,3);
+
+}())
